f30edb015b57875b8a3f3cb0e68397085bffd867
Julian <36317786+EnJulian@users.noreply.github.com>
2023-07-12 09:26:03 +0000

    final commit

--- exercise-2.js
+++ exercise-2.js
@@ -4,9 +4,11 @@ let quantity = 5;
 console.log(`Initial price: $${price} with quantity: ${quantity}`)
 
 let discount = 20/100;
-newPrice = price - (price * discount);
+
+let newPrice = 90;
+    newPrice -= price * discount;
 quantity++;
-console.log(`Calculated Discount: ${newPrice}`);
+console.log(`New Price: ${newPrice}`);
 
 totalPrice = quantity * newPrice;
 console.log(`Item A has a quantity of ${quantity} and price of $${newPrice} with a discount of 20%. Total Price is: $${totalPrice}`);
